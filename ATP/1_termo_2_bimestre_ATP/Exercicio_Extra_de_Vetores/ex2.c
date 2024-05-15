8e3ae698d7d65a928dbc3d8db84b14ed4e9006d7
felipe.akio.nishimura <felipe.akio.nishimura@gmail.com>
2024-05-15 18:59:17 -0300

    lista do chico

--- ATP/1_termo_2_bimestre_ATP/Exercicio_Extra_de_Vetores/ex2.c
+++ ATP/1_termo_2_bimestre_ATP/Exercicio_Extra_de_Vetores/ex2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #define TF 5
 int main(){
-    int numVoo[TF], lugDisp[TF], idem, voo, res
-    for(int i=0;i<TF;i++){
+    int numVoo[TF], lugDisp[TF], idem, voo, res, bo, i;
+    for(i=0;i<TF;i++){
         printf("Digite o numVoo:\n");
         scanf("%d", &numVoo[i]);
     }
-    for(int i=0;i<TF;i++){
+    for(i=0;i<TF;i++){
         printf("Digite o lugares disponiveis do numVoo %d\n", numVoo[i]);
         scanf("%d", &lugDisp[i]);
     }
@@ -15,19 +15,25 @@ int main(){
     while(idem!=9999){
         printf("Digite o numero de voo:\n");
         scanf("%d", &voo);
-        for(int i=0;i<TF;i++){
+        for(i=0;i<TF;i++){
             if(numVoo[i]==voo){
                 lugDisp[i]--;
-                res= lugDisp[i];
+                res = lugDisp[i];
+                bo=1;
                 goto label;
             }
             else{
-                printf("numero invalido\n");
+                bo=0;
             }
         }
         label:
-        printf("numero de vagas:%d\n", res);
+        if(bo){
+            printf("numero de vagas:%d\n", res);
+        }
+        else
+            printf("numero invalido\n");
         printf("Digite o identidade:\n");
         scanf("%d", &idem);
     }
+    return 0;
 }
