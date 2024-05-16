6d22b71f2d9ed1c16f0771c041dcdcf11e37adf7
felipeakioxxx <152916454+oregonyuky@users.noreply.github.com>
2024-05-16 11:21:51 -0300

    Update ex2.c

--- ATP/1_termo_2_bimestre_ATP/Exercicio_Extra_de_Vetores/ex2.c
+++ ATP/1_termo_2_bimestre_ATP/Exercicio_Extra_de_Vetores/ex2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define TF 5
 int main(){
-    int numVoo[TF], lugDisp[TF], idem, voo, res, bo, i;
+    int numVoo[TF], lugDisp[TF], idem, voo, res, bo, i, pos;
     for(i=0;i<TF;i++){
         printf("Digite o numVoo:\n");
         scanf("%d", &numVoo[i]);
@@ -13,25 +13,17 @@ int main(){
     printf("Digite o identidade:\n");
     scanf("%d", &idem);
     while(idem!=9999){
+		pos=0;
         printf("Digite o numero de voo:\n");
         scanf("%d", &voo);
-        for(i=0;i<TF;i++){
-            if(numVoo[i]==voo){
-                lugDisp[i]--;
-                res = lugDisp[i];
-                bo=1;
-                goto label;
-            }
-            else{
-                bo=0;
-            }
-        }
-        label:
-        if(bo){
-            printf("numero de vagas:%d\n", res);
-        }
-        else
-            printf("numero invalido\n");
+			while(pos<TF && numVoo[pos]!=voo)
+				pos++;
+			if(pos==TF)
+				printf("Nao foi encontrado\n");
+			else{
+				lugDisp[pos]--;
+				printf("numero de vagas:%d\n", lugDisp[pos]);
+			}
         printf("Digite o identidade:\n");
         scanf("%d", &idem);
     }
