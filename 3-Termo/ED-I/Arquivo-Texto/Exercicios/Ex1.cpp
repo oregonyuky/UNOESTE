52915b9848a131f7b92b6cbffa522dfce149af33
felipe akio nishimura <felipe.akio.nishimura@gmail.com>
2025-02-17 15:50:35 -0300

    lista

--- 3-Termo/ED-I/Arquivo-Texto/Exercicios/Ex1.cpp
+++ 3-Termo/ED-I/Arquivo-Texto/Exercicios/Ex1.cpp
@@ -1,5 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
-void addLetter(char inFIle, char outFIle){
-
+void to_uppercase_file(char inputFile, char outputFile){
+    FILE *entrada = fopen(inputFile, "r");
+    if(entrada == NULL){
+        printf("Erro ao abrir arquivo de entrada\n");
+        return;
+    }
+    FILE *output = fopen(outputFile, "w");
+    if(output == NULL){
+        printf("Erro ao abrir arquivo de saida\n");
+        return;
+    }
+    int c;
+    while(!feof(entrada)){
+        c = fgetc(entrada);
+        if(!feof(entrada)){
+            fputc(toupper(c), output);
+        }
+    }
+    fclose(entrada);
+    fclose(output);
+}
+int main(){
+    char entrada[256], output[256];
+    printf("Digite o nome do arquivo de entrada: ");
+    scanf("%255s", entrada);
+    printf("Digite o nome do arquivo de saida: ");
+    scanf("%255s", output);
+    to_uppercase_file(entrada, saida);
+    printf("Arquivo convertido com sucesso.\n");
+    return 0;
 }
---
+++ 3-Termo/GP/README.md
@@ -0,0 +1,3 @@
+## Erros
+Os Erros nos processos da matematica computacional podem surgir de diversas fontes. Coleta de dados e definicao do
+modelo matematico sao exemplos. Na etapa de implementacao pode-se ter basicamente os erros de resolucao e discretizacao.
