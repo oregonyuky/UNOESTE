3654f90c37906a240f672f5e777f276da912fb31
felipe.akio.nishimura <felipe.akio.nishimura@gmail.com>
2025-02-17 09:09:59 -0300

    lista

---
+++ 3-Termo/LP-I/AULA-3/scripts.js
@@ -0,0 +1,21 @@
+var t1 = "oi";
+var t2;
+var r3 = null; // para objetos
+if(t1 == undefined)
+{
+    //document e o objeto que representa a pagina html
+    document.write("<span> t1 e undefined</span>");
+}
+if(t2 == undefined)
+{
+    //document e o objeto que representa a pagina html
+    document.write("<span> t2 e undefined</span>");
+}
+if(typeof t1 == "string")
+{
+    document.write("<span> t1 e uma string</span>");
+}
+if(typeof t3 == "object")
+{
+    document.write("<span> t3 e um objeto </span>");
+}
