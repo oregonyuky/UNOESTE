545651e4ff4642a251f07a3940a86782e8e67f57
felipe akio nishimura <felipe.akio.nishimura@gmail.com>
2025-02-17 23:54:46 -0300

    lista

--- 3-Termo/ED-I/Arquivo-Texto/Exercicios/Ex2.cpp
+++ 3-Termo/ED-I/Arquivo-Texto/Exercicios/Ex2.cpp
@@ -1,2 +1,19 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <ctype.h>
+void Quntidade_de_letras_espacos_linhas(char inFile[30], char outFile[30]){
+    FILE *entrada = fopen(inFile, "r");
+    if(entrada == NULL){
+        printf("Erro ao abrir o arquivo\n");
+        return;
+    }
+    FILE *saida = fopen(outFile, "w");
+    if(saida == NULL){
+        printf("Erro ao abrir o arquivo\n");
+        return;
+    }
+    int c;
+    while(!feof(entrada)){
+        c = 
+    }
+}
