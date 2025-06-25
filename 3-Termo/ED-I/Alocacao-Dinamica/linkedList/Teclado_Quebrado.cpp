7ed3a991d833008d264fa495cf1bcb41f187e66d
Felipe Akio Nishimura <felipe.akio.nishimura@gmail.com>
2025-06-25 01:43:17 -0300

    Resolvido

--- 3-Termo/ED-I/Alocacao-Dinamica/linkedList/Teclado_Quebrado.cpp
+++ 3-Termo/ED-I/Alocacao-Dinamica/linkedList/Teclado_Quebrado.cpp
@@ -4,160 +4,213 @@
 #include <iostream>
 #define dbg(v) cout << #v << " = " << (v) << endl 
 using namespace std;
-struct TpPont{
-    char v;
-    TpPont *ant, *prox;
-};
-struct TpPont1{
-    const char *v;
-    TpPont1 *ant, *prox;
-};
-TpPont *novaCaixa(char v){
-    TpPont *aux = new TpPont;
-    aux->v = v;
-    aux->ant = NULL;
-    aux->prox = NULL;
-    return aux;
-};
-TpPont *InserirFim(TpPont *lis, char v){
-    TpPont *aux = lis, *nc = novaCaixa(v);
-    if(!aux)return novaCaixa(v);
-    while(aux->prox)aux = aux->prox;
-    aux->prox = novaCaixa(v);
-    aux->prox->ant = aux;
-    return lis;
-}
-TpPont1 *novaCaixaV(const char *v){
-    TpPont1 *aux = new TpPont1;
-    aux->v = v;
-    aux->ant = NULL;
-    aux->prox = NULL;
-    return aux;
-};
-TpPont1 *InserirFimV(TpPont1 *lis, const char *v){
-    TpPont1 *aux = lis;
-    if(!aux)return novaCaixaV(v);
-    while(aux->prox)aux = aux->prox;
-    aux->prox = novaCaixaV(v);
-    aux->prox->ant = aux;
-    return lis;
-}
-char *Juntar(TpPont *lis){
-    TpPont *aux = lis;
-    char *nome = new char;
-    strcpy(nome, "");
-    char temp[2];
-    while(aux){
-        temp[0] = aux->v;
-        temp[1] = '\0';
-        strcat(nome, temp);
-        aux = aux->prox;
-    }
-    return nome;
-}
-void Exibir(TpPont1 *lis){
-    TpPont1 *aux = lis;
-    while(aux->prox)aux = aux->prox;
-    while(aux){
-        printf("%s", aux->v);
-        aux = aux->ant;
-    }
-    puts("");
-}
-TpPont *Excluir(TpPont *lis, int i){
-    TpPont *aux = lis;
-    while(aux && i--)aux = aux->prox; 
-    if(!aux)return lis;
-    if(!aux->ant){
-        aux = aux->prox;
-        delete aux;
+class FelipeAkio{
+    public:
+    struct TpPont{ 
+        char v;
+        TpPont *ant, *prox;
+    };
+    struct TpPont1{
+        char v;
+        TpPont1 *ant, *prox;
+    };
+    TpPont *novaCaixa(char v){
+        TpPont *aux = new TpPont;
+        aux->v = v;
+        aux->ant = NULL;
+        aux->prox = NULL;
+        return aux;
+    };
+    TpPont *InserirFim(TpPont *lis, char v){
+        TpPont *aux = lis, *nc = novaCaixa(v);
+        if(!aux)return novaCaixa(v);
+        while(aux->prox)aux = aux->prox;
+        aux->prox = novaCaixa(v);
+        aux->prox->ant = aux;
         return lis;
     }
-    else if(!aux->prox){
-        aux->ant->prox = NULL;
-        delete(aux);
-        return lis;
-    }else{
-        aux->ant->prox = aux->prox;
-        aux->prox->ant = aux->ant;
+    TpPont1 *novaCaixaV(char v){
+        TpPont1 *aux = new TpPont1;
+        aux->v = v;
+        aux->ant = NULL;
+        aux->prox = NULL;
+        return aux;
+    };
+    TpPont1 *InserirFimV(TpPont1 *lis, char v){
+        TpPont1 *aux = lis;
+        if(!aux)return novaCaixaV(v);
+        while(aux->prox)aux = aux->prox;
+        aux->prox = novaCaixaV(v);
+        aux->prox->ant = aux;
         return lis;
     }
-}
-void teste(){
-    char str[100]; 
-    TpPont *lis = NULL;
-    FILE *arq = fopen("input.txt", "r");
-    while(fscanf(arq, "%[^\n]\n", str)!=EOF){
-        if (strcmp(str, "") == 0) break;
-        TpPont *lis = NULL; TpPont1 *aux=NULL;
-        for(int i=0;str[i]!='\0';i++){
-            if(str[i]=='['){
-                while(++i <strlen(str)){
-                    if(str[i]==']')break;   
-                    if(str[i]=='['){i--;break;}
-                    //dbg(i);
-                    lis = InserirFim(lis, str[i]);
-                }   
-            }
+    TpPont *Juntar(TpPont *lis){
+        TpPont *aux = lis, *exp = NULL;
+        while(aux){
+            exp = InserirFim(exp, aux->v);
+            aux = aux->prox;
         }
-        //printf("%s\n", Juntar(lis));
-        aux = InserirFimV(aux, Juntar(lis));
-        Exibir(aux);printf("%s\n", str);
+        return exp;
     }
-    fclose(arq);
-}
-void teste2(){
-    freopen("input.txt", "r", stdin);
-    char str[100]; 
-    TpPont *lis = NULL;
-     TpPont1 *aux = NULL;
-    while(fgets(str, sizeof(str), stdin)){
-        if (strcmp(str, "") == 0) break;
-        TpPont *lis = NULL, *todo=NULL;
-        for(int i=0;str[i]!='\0';i++)
-            todo = InserirFim(todo, str[i]);
-        for(int i=0;str[i]!='\0';i++){
-            if(str[i]=='['){
-                todo = Excluir(todo, i);
-                lis = NULL;
-                while(++i <strlen(str)){
-                    if(str[i]==']'){
-                        todo = Excluir(todo, i);
-                        break;
-                    } 
-                    if(str[i]=='['){
-                        todo = Excluir(todo, i);
-                        i--;
-                        break;
+    void Exibir(TpPont1 *lis){
+        TpPont1 *aux = lis;
+        while(aux->prox)aux = aux->prox; 
+        while(aux){
+            printf("%c", aux->v);
+            aux = aux->ant;
+        }
+    }
+    TpPont *Inverter(TpPont *lis){
+        TpPont *aux = lis, *aux1=NULL;
+        while(aux->prox)aux = aux->prox;
+        while(aux){
+            aux1 = InserirFim(aux1, aux->v);
+            aux = aux->ant;
+        }
+        return aux1;
+    }
+    TpPont *Excluir(TpPont *lis, int i){
+        TpPont *aux = lis;
+        while(aux && i--)aux = aux->prox; 
+        if(!aux)return lis;
+        if(!aux->ant){
+            lis = aux->prox;
+            if (lis) lis->ant = NULL;
+            delete aux;
+            return lis;
+        }
+        else if(!aux->prox){
+            aux->ant->prox = NULL;
+            delete(aux);
+            return lis;
+        }else{
+            aux->ant->prox = aux->prox;
+            aux->prox->ant = aux->ant;
+            delete aux;
+            return lis;
+        }
+    }
+    void teste2(){
+        freopen("input.txt", "r", stdin);
+        char str[100005]; 
+        TpPont *lis = NULL;
+         TpPont1 *aux = NULL;
+        while(fgets(str, sizeof(str), stdin)){
+            if (strcmp(str, "") == 0) break;
+            TpPont *lis = NULL, *todo=NULL; TpPont1 *aux=NULL;
+            for(int i=0;str[i]!='\0';i++){
+                if(str[i]=='['){
+                    lis = NULL;
+                    while(++i <strlen(str)){
+                        if(str[i]==']'){
+                            break;
+                        }
+                        if(str[i]=='['){
+                            i--;
+                            break;
+                        }
+                        if(str[i]!='\n')
+                            lis = InserirFim(lis, str[i]);
+                    }   
+                    if(lis)
+                        lis = Inverter(lis);
+                    while(lis){
+                        aux = InserirFimV(aux, lis->v);
+                        lis = lis->prox;
                     }
-                    lis = InserirFim(lis, str[i]);
-                    todo = Excluir(todo, i);
-                }   
-                aux = InserirFimV(aux, Juntar(lis));
-                //printf("%s\n", Juntar(lis));
+                }
+                else if(str[i]!=']'){
+                    todo = InserirFim(todo, str[i]);
+                }
+            }
+            if (aux) {
+                while(aux->prox) aux = aux->prox; 
+                while(aux){
+                    printf("%c", aux->v);
+                    aux = aux->ant;
+                }
+            }
+            while(todo){
+                printf("%c", todo->v);
+                todo = todo->prox;
             }
         }
-        printf("%s\n", str);
-        Exibir(aux);
-        while(todo){
-            printf("%c", todo->v);
-            todo = todo->prox;
+    }
+};
+class Resolvido{
+    public:
+    struct TpPont {
+        char v;
+        TpPont *ant, *prox;
+    };
+
+    TpPont *novaCaixa(char v) {
+        TpPont *aux = new TpPont;
+        aux->v = v;
+        aux->ant = aux->prox = NULL;
+        return aux;
+    }
+
+    void liberar(TpPont *p) {
+        while (p) {
+            TpPont *tmp = p;
+            p = p->prox;
+            delete tmp;
         }
     }
-}
-void teste3(){
-    TpPont *lis = NULL;
-    lis = InserirFim(lis, 'a');
-    lis = InserirFim(lis, 'b');
-    lis = InserirFim(lis, 'c');
-    lis = InserirFim(lis, 'd');
-    lis = InserirFim(lis, 'e');
-    lis = Excluir(lis, 3);
-    while(lis){
-        printf("%c ", lis->v);
-        lis = lis->prox;
+    void imprimir(TpPont *ini) {
+        while (ini) {
+            putchar(ini->v);
+            ini = ini->prox;
+        }
+        putchar('\n');
     }
-}
+    void teste() {
+        char str[100005];
+        while (fgets(str, sizeof(str), stdin)) {
+            TpPont *ini = NULL, *fim = NULL, *cursor = NULL;
+            for (int i = 0; str[i]; i++) {
+                char c = str[i];
+                if (c == '\n') continue;
+                if (c == '[') {
+                    cursor = NULL; 
+                } else if (c == ']') {
+                    cursor = fim; 
+                } else {
+                    TpPont *novo = novaCaixa(c);
+                    if (!ini) {
+                        ini = fim = novo;
+                        cursor = novo;
+                    } else if (cursor == NULL) { 
+                        novo->prox = ini;
+                        ini->ant = novo;
+                        ini = novo;
+                        cursor = novo;
+                    } else if (cursor == fim) { 
+                        fim->prox = novo;
+                        novo->ant = fim;
+                        fim = novo;
+                        cursor = novo;
+                    } else { // meio
+                        novo->prox = cursor->prox;
+                        novo->ant = cursor;
+                        if (cursor->prox)
+                            cursor->prox->ant = novo;
+                        cursor->prox = novo;
+                        cursor = novo;
+                        if (!novo->prox) fim = novo;
+                    }
+                }
+            }
+            imprimir(ini);
+            liberar(ini);
+        }
+    }
+};
+
 int main(){
-    teste2();
+    FelipeAkio f;
+    Resolvido c;
+    c.teste();
+    //f.teste2();
 }
