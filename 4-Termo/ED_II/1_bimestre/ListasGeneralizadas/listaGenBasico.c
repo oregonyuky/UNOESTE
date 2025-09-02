e86a5091bb8d564a117bf4c8a9dcf010e8ab55d9
oregonyuky <felipe.akio.nishimura@gmail.com>
2025-09-02 11:59:06 -0300

    need to be fixed

--- 4-Termo/ED_II/1_bimestre/ListasGeneralizadas/listaGenBasico.c
+++ 4-Termo/ED_II/1_bimestre/ListasGeneralizadas/listaGenBasico.c
@@ -16,7 +16,34 @@ struct listagen
 	char terminal;
 	union info_lista no;
 };
+struct pilha{
+	ListaGen *l;
+	struct Pilha *prox;
+}
+typedef struct pilha Pilha;
 typedef struct listagen ListaGen;
+void init(Pilha **p){
+	*p == NULL;
+}
+void push(Pilha **p, ListaGen *l){
+	Pilha *aux = (Pilha*)malloc(sizeof(Pilha));
+	aux->l = l;
+	aux->prox = *p;
+	*p = aux;
+}
+void pop(Pilha **p, ListaGen *l){
+	Pilha *aux;
+	if(!isEmpty(*p)){
+        *l = (*p)->l;
+        aux = *p;
+        *p = aux->prox;
+        free(aux);
+    }
+    else{
+        *l = -1;
+    }
+}
+
 ListaGen *CriaT(char *info)
 {
 	ListaGen *L = (ListaGen*)malloc(sizeof(ListaGen));
@@ -68,9 +95,79 @@ void Exibir(ListaGen *l){
 		
 	}	
 }
+void ExibirAtomo(ListaGen *l){
+	if(!Nula(l)){
+		if(Atomo(l))
+			printf("%s", l->no.info);
+		else{
+			ExibirAtomo(Head(l));
+			ExibirAtomo(Tail(l));
+		}
+	}
+}
+void ExibirAtomoI(ListaGen *l){
+	Pilha *p;
+	init(&p);
+	push(&p, l);
+	while(!isEmpty(p)){
+		if(!Nula(l)){
+			pop(&p, &l);
+			while(!Nula(l) && !Atomo(l)){
+				push(&p, l);
+				l = Head(l);
+			}
+			if(Atomo(l)){
+				printf("%s", l->no.info);
+				
+			}
+		}
+		pop(&p, &l);
+		l = Tail(l);
+		if(!Nula(l))
+			push(&p, l);
+	}
+}
+void Destruir(ListaGen **l){
+	if(!Nula(*l)){
+		if(Atomo(*l)){
+			free(*l);
+		}
+		else{
+			destruir(Head(&(*l)->no.lista.cabeca));
+			destruir(Tail(&(*l)->no.lista.cauda));
+			free(*l);
+			*l = NULL;
+		}
+	}
+}
+void DestruirI(ListaGen **l){
+	Pilha *p;
+	init(&p);
+	push(&p, *l);
+	while(!isEmpty(p)){
+		if(!Nula(*l)){
+			pop(&p, &*l);
+			while(!Nula(*l) && !Atomo(*l)){
+				push(&p, *l);
+				*l = Head(*l);
+			}
+			if(Atomo(*l)){
+				free(*l);
+			}
+		}
+		pop(&p, &l);
+		aux = *l;
+		*l = Tail(*l);
+		free(aux);
+		if(!Nula(*l))
+			push(&p, *l);
+	}
+}
 int main(){
 	//[a, [b, c]]
 	ListaGen *L;
 	L = Cons(CriaT("a"), Cons(Cons(CriaT("b"), Cons(CriaT("c"), NULL)), NULL));
-	Exibir(L);
+	Exibir(L); puts("");
+	ExibirAtomo(L); puts("");
+	ExibirAtomoI(L);
 }
